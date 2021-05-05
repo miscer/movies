763105973dc6ec3ed9c9191bf0d7afd879e78e04
Michal Miškerník <michal@miskernik.sk>
2021-05-05 19:51:02 +0200

    Movie list component (closes #2)

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,72 @@
 <template>
   <div id="app">
-    <Movie
-      title="The Notebook"
-      :release-date="new Date('2004-06-25T00:00:00.000000Z')"
-      :genres="['Drama', 'Romance']"
-      :actors="['Ryan Gosling', 'Rachel McAdams']"
-    />
+    <MovieList :movies="movies" />
   </div>
 </template>
 
 <script>
-import Movie from "@/components/Movie.vue";
+import MovieList from "@/components/MovieList.vue";
 
 export default {
   name: "App",
   components: {
-    Movie,
+    MovieList,
+  },
+  data() {
+    return {
+      movies: [
+        {
+          id: 163979287265942016,
+          title: "An American Tail: Fievel Goes West",
+          genre: [
+            {
+              id: 3,
+              title: "Comedy",
+            },
+            {
+              id: 8,
+              title: "Western",
+            },
+          ],
+          actors: [
+            {
+              id: 163978496991957504,
+              name: "Fievel Mousekewitz",
+            },
+            {
+              id: 163978647315813376,
+              name: "Tanya MouseKewitz",
+            },
+          ],
+          releaseDate: new Date("1991-11-17T00:00:00.000000Z"),
+        },
+        {
+          id: 163982121944357888,
+          title: "The Notebook",
+          genre: [
+            {
+              id: 2,
+              title: "Drama",
+            },
+            {
+              id: 6,
+              title: "Romance",
+            },
+          ],
+          actors: [
+            {
+              id: 163982182073900544,
+              name: "Ryan Gosling",
+            },
+            {
+              id: 163982199253770240,
+              name: "Rachel McAdams",
+            },
+          ],
+          releaseDate: new Date("2004-06-25T00:00:00.000000Z"),
+        },
+      ],
+    };
   },
 };
 </script>
---
+++ src/components/MovieList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="movie-list">
+    <Movie
+      v-for="movie in movies"
+      :key="movie.id"
+      :title="movie.title"
+      :release-date="movie.releaseDate"
+      :genres="movie.genre.map((g) => g.title)"
+      :actors="movie.actors.map((a) => a.name)"
+    />
+  </div>
+</template>
+
+<script>
+import Movie from "@/components/Movie.vue";
+
+export default {
+  name: "MovieList",
+  components: {
+    Movie,
+  },
+  props: {
+    movies: Array,
+  },
+};
+</script>
+
+<style scoped>
+.movie-list {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-row-gap: 40px;
+  grid-column-gap: 40px;
+}
+</style>
