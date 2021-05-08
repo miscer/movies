2e94b8f94e6a3553808e6dfed99adacf92c46bfc
Michal Miškerník <michal@miskernik.sk>
2021-05-08 20:34:28 +0200

    Styled movie component

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,9 +1,15 @@
 <template>
-  <article>
-    <img :src="posterUrl" :alt="'Poster for ' + title" />
-    <h3>{{ title }} ({{ releaseYear }})</h3>
-    <div>Genres: {{ genres.join(", ") }}</div>
-    <div>Actors: {{ actors.join(", ") }}</div>
+  <article class="movie">
+    <img class="poster" :src="posterUrl" :alt="'Poster for ' + title" />
+    <h3 class="title">{{ title }} ({{ releaseYear }})</h3>
+    <article class="info">
+      <ul class="genres">
+        <li v-for="(genre, index) in genres" :key="index">{{ genre }}</li>
+      </ul>
+      <ul class="actors">
+        <li v-for="(actor, index) in actors" :key="index">{{ actor }}</li>
+      </ul>
+    </article>
   </article>
 </template>
 
@@ -25,4 +31,76 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style lang="less" scoped>
+.movie {
+  position: relative;
+  overflow: hidden;
+}
+
+.poster {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+
+.title {
+  display: block;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: 0;
+  padding: 40px 20px 20px;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0) 0%,
+    rgba(0, 0, 0, 0.8) 100%
+  );
+  font-size: 1.5rem;
+  color: #fff;
+}
+
+.info {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  padding: 20px;
+  transition: transform 200ms ease-in-out;
+  background-color: rgba(0, 0, 0, 0.8);
+  color: white;
+
+  .movie:hover & {
+    transform: translateY(-100%);
+  }
+}
+
+.genres {
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  padding: 0;
+  margin: 0 0 20px;
+
+  li {
+    display: block;
+    padding: 5px;
+    margin-right: 10px;
+    margin-bottom: 10px;
+    color: black;
+    background-color: rgba(255, 255, 255, 0.9);
+  }
+}
+
+.actors {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  font-size: 1.2rem;
+
+  li {
+    margin-bottom: 10px;
+  }
+}
+</style>
