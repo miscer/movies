8379155a59fdb0b700e2cb24fdc30bb9d6a6dce9
Michal Miškerník <michal@miskernik.sk>
2021-05-09 22:03:26 +0200

    Use JSDOM v16 when running tests to support needed features

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
+  testEnvironment: "jest-environment-jsdom-sixteen",
   setupFilesAfterEnv: ["<rootDir>/jest-setup.js"],
   transform: {
     "^.+\\.vue$": "vue-jest",
